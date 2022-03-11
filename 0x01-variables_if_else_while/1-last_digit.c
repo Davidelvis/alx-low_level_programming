8a3706eed545364273947d906ee7eb1248d6d87e
Davidelvis <komboelvis08@gmail.com>
2022-03-11 02:10:42 -0800

    C variables and control statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: ALways 0 (Success)
+ * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 {
 	int n;
 
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ((n % 10) == 0)
-		printf("Last digit of %d is %d and is 0/n", n, n % 10);
+
+	if ((n % 10) ==  0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else if ((n % 10) > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	else
@@ -25,4 +26,3 @@ int main(void)
 	return (0);
 
 }
-
