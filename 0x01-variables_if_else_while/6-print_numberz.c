d504c2937c9638ccacd6337d4ad7159098c8ac22
Davidelvis <komboelvis08@gmail.com>
2022-03-11 03:40:50 -0800

    C variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		putchar(d + '0');
+	putchar('\n');
+
+return (0);
+}
