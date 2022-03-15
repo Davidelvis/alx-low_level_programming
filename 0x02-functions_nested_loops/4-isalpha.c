5bc80372bc00119d541379f7a2e499cd16bc6d81
Davidelvis <komboelvis08@gmail.com>
2022-03-15 13:37:26 -0700

    C functions

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,21 @@
 #include "main.h"
-
 /**
- * _isalpha - check main
- * @c: An input character
- * Description: function that returns 1 if the character is a
- * letter, lowrcase or uppercase.
- * Return: 1 or 0 in otherwise
- */
+* _isalpha - Shows 1 if the input is c
+* letter Another cases, shows 0
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for letters. Returns 0 otherwise
+*/
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
-
-	for (lower = 'a'; lower <= 'z; lower++)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (c == lower || c == upper)
-				isletter = 1;
-		}
+	return (1);
 	}
-return (isletter);
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
 }
