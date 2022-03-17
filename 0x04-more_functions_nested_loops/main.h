58a6fd8bfd3ef1cca19c91b090a89821ceb91930
Davidelvis <komboelvis08@gmail.com>
2022-03-17 09:43:20 -0700

    Header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_FILE
-#define MAIN_FILE
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,9 +6,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-
-
-
-
-
-#endif
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
