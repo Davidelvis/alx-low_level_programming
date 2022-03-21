4dba7f8e655ed0db9edcc031a9def8575ff684ab
Davidelvis <komboelvis08@gmail.com>
2022-03-21 08:04:28 -0700

    Pointers in C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98: Function that takes a pointer and updates the value
+ * @n: int input pointer
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
